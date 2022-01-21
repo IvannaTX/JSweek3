7b35c6e97cacd287733974d68cc9512422bcd5a8
Ivanna Contreras <ivanna.aws@gmail.com>
2022-01-21 07:42:56 -0600

    updated some challenges

---
+++ Easy/easy.js
@@ -0,0 +1,37 @@
+/*EASY: Write a function that would allow you to do this:
+
+var run = exercise('running');
+
+console.log(run()); // prints "Today's exercise: running"
+
+var swim = exercise('swimming');
+
+console.log(swim()); // prints "Today's exercise: swimming"*/
+
+
+/*let run = exercise('running');
+
+function exercise () {
+    console.log("Today's exercise: running");
+}
+
+let swim = exercise2('swimming');
+
+function exercise2 () {
+    console.log("Today's exercise: swimming");
+}*/
+
+let run = exercise('running');
+let swim = exercise('swimming');
+
+function exercise () {
+    console.log("Today's exercise: running");
+    console.log("Today's exercise: swimming");
+}
+
+
+/*let run = () => {
+    console.log("Today's exercise: swimming")
+}*/
+
+
---
+++ Medium/medium.js
@@ -0,0 +1,20 @@
+/*MEDIUM: Write a function that would allow you to do this:
+
+var sharePizza = cutPizzaSlices(8);
+
+console.log(sharePizza(2));                                                                                                                                                                    
+
+  // prints "Each person gets 4.00 slices of pizza"
+
+console.log(sharePizza(3)); 
+
+  // prints "Each person gets 2.67 slices of pizza"*/
+
+
+
+var sharePizza = cutPizzaSlices(8);
+
+function cutPizzaSlices() {
+    console.log("Each person gets 4.00 slices of pizza")
+}
+
